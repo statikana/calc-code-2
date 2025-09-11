7e259cc1301337b5fa755c20aae897d1542cd6af
statikana <86629697+statikana@users.noreply.github.com>
2025-09-11 19:03:07 -0400

    some changes?

--- objects/structres_root/constant.py
+++ objects/structres_root/constant.py
@@ -1,27 +1,33 @@
+from __future__ import annotations
+
+from types import NotImplementedType
 from typing import Any, overload, Self
 from node import *
+
+__all__ = ["Constant"]
 
 
 class Constant(Node):
     """Single number."""
+
     def __init__(self, value: float):
         self.value = value
 
-    
     @overload
-    def __add__(self, other: Self) -> Self:
-        ...
-    
-    @overload
-    def __add__(self, other: NodeT) -> Add[Self, NodeT]:
-        ...
+    def __add__(self, other: Node) -> Add[Self, Node]: ...
 
-    def __add__(self, other: Self | NodeT) -> Self | Add[Self, NodeT]:
+    @overload
+    def __add__(self, other: Self) -> Self: ...
+
+
+    def __add__(self, other: object) -> Self | Add[Self, Node] | NotImplementedType:
         match other:
             case Constant():
                 return Constant(self.value + other.value)
-            case _:
+            case Node():
                 return Add(self, other)
+            case _:
+                return NotImplemented
 
     def __sub__(self, other: NodeT) -> "Constant | Sub[Constant, NodeT]":
         match other:
@@ -47,10 +53,10 @@ class Constant(Node):
     def __pow__(self, other: NodeT) -> "Constant | Pow[Constant, NodeT]":
         match other:
             case Constant():
-                return Constant(self.value ** other.value)
+                return Constant(self.value**other.value)
             case _:
                 return super().__pow__(other)
 
+
 C = Constant
 
-a = Constant(3) + Constant(5)
--- objects/structres_root/structures_1/variable.py
+++ objects/structres_root/structures_1/variable.py
@@ -1,7 +1,8 @@
 from node import *
 from ..constant import Constant
 
+
 class Variable(Node):
     def __init__(self, name: str, value: Constant | None = None):
-        self.name=name
+        self.name = name
         self.value = value
